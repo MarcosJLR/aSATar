5b2d5712c51209e3e6103bfa9cd5ce12282c1894
MarcosJLR <socram.693@gmail.com>
2020-06-28 19:47:20 -0400

    Added a main to run the solver

---
+++ src/solver/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <fstream>
+
+#include "Solver.hpp"
+
+using namespace asatar;
+using namespace std;
+
+Solver solver;
+
+int main(int argc, char* argv[])
+{
+	int timeout = 0;
+	if(argc > 1)
+	{
+		timeout = atoi(argv[1]);
+	}
+
+	if(timeout > 0) { solver.setTimeout(timeout); }
+
+	solver.readFromFile(cin);
+	solver.solve();
+	solver.printToFile(cout);
+
+	return 0;
+}
