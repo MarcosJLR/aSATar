b8c853aa0fbf7c814c555e9fa87806f6c61fe143
MarcosJLR <socram.693@gmail.com>
2020-07-14 23:20:39 -0400

    Fixed scripts

--- run_aSatar.sh
+++ run_aSatar.sh
@@ -1,15 +1,14 @@
 rm ./files/aSatar.txt
 rm ./files/aSatar_sudokus/*.png
 echo "Ingrese el nombre del archivo con los sudokus a resolver:"
-read fileaddress
-cp $fileaddress ./files/Instances.txt
+cp $1 ./files/Instances.txt
 filesdir="$PWD/files"
 make -C ./src/sudoku/
 make -C ./src/solver/
 while read line; do
 	if [[ $line = [0-9]* ]]; then
 		cd ./src/sudoku 
-		./sudsat "$filesdir/sat.txt" "$line"
+		./sud_sat "$filesdir/sat.txt" "$line"
 		cd ..
 		cd ./solver
 		./solver "$filesdir/sat.txt"
@@ -17,11 +16,11 @@ while read line; do
 		rm ./solution.txt
 		cd ..
 		cd ./sudoku
-		./satsud "$filesdir/solution.txt" "$filesdir/aSatar.txt"
+		./sat_sud "$filesdir/solution.txt" "$filesdir/aSatar.txt"
 		cd ..
 		cd ..
 	fi
-done < $fileaddress
+done < $1
 cd ./src/
 python3 plots.py "$filesdir/aSatar.txt"
 make clean -C ./solver/
--- run_zchaff.sh
+++ run_zchaff.sh
@@ -1,15 +1,14 @@
 rm ./files/zchaff.txt
 rm ./files/zchaff_sudokus/*.png
 echo "Ingrese el nombre del archivo con los sudokus a resolver:"
-read fileaddress
-cp $fileaddress ./files/Instances.txt
+cp $1 ./files/Instances.txt
 filesdir="$PWD/files"
 make -C ./src/sudoku/
 make -C ./src/zchaff64/
 while read line; do
 	if [[ $line = [0-9]* ]]; then	
 		cd ./src/sudoku 
-		./sudsat "$filesdir/sat.txt" "$line"
+		./sud_sat "$filesdir/sat.txt" "$line"
 		cd ..
 		cd ./zchaff64
 		./zchaff "$filesdir/sat.txt"
@@ -17,11 +16,11 @@ while read line; do
 		rm ./solution.txt
 		cd ..
 		cd ./sudoku
-		./satsud "$filesdir/solution.txt" "$filesdir/zchaff.txt"
+		./sat_sud "$filesdir/solution.txt" "$filesdir/zchaff.txt"
 		cd ..
 		cd ..
 	fi
-done < $fileaddress
+done < $1
 cd ./src/
 python3 plots.py "$filesdir/zchaff.txt"
 make clean -C ./zchaff64/
